9c00a6c4ec430466e9afcdee263d208287a6db0c
Anwesh43 <anweshthecool0@gmail.com>
2021-06-07 02:09:29 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -25,3 +25,38 @@ class ScaleUtil {
     }
 }
 
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawRotateLineToEitherSide(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2)
+        context.save()
+        context.rotate(Math.PI * sc3)
+        context.translate(size / 10, -h / 2 - size + (h / 2 + size) * (sc1 - sc4))
+        DrawingUtil.drawLine(context, 0, 0, 0, size)
+        context.restore()
+        context.fillRect(-size / 2 + size * sc4, -size / 2, size * sc1, size)
+        context.restore()
+    }
+
+    static drawRLTESNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.fillStyle = colors[i]
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawRotateLineToEitherSide(context, scale)
+    }
+}
